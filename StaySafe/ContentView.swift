ab710416352bbb94738380f728f72fed5b5bdb87
stud <binaspaulina1@gmail.com>
2019-08-01 21:31:13 +0200

    Project Initialization

---
+++ StaySafe/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  StaySafe
+//
+//  Created by stud on 01/08/2019.
+//  Copyright © 2019 stud. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
